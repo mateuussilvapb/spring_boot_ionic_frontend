5841a2d2c86af19f8aa18ef506b240d3c002a839
Mateus da Silva Sousa Dias <mateuussilvapb@gmail.com>
2022-05-30 14:54:14 -0300

    Desabilitar menu na tela inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,27 +3,33 @@ import { Nav, Platform } from "ionic-angular";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
 
+// ================================================= //
 @Component({
   templateUrl: "app.html",
 })
+// ================================================= //
 export class MyApp {
+  // ================================================= //
   @ViewChild(Nav) nav: Nav;
 
+  // ================================================= //
   rootPage: string = "HomePage";
 
+  // ================================================= //
   pages: Array<{ title: string; component: string }>;
 
+  // ================================================= //
   constructor(
     public platform: Platform,
     public statusBar: StatusBar,
     public splashScreen: SplashScreen
   ) {
     this.initializeApp();
-
     // used for an example of ngFor and navigation
     this.pages = [{ title: "Home", component: "HomePage" }];
   }
 
+  // ================================================= //
   initializeApp() {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
@@ -33,6 +39,7 @@ export class MyApp {
     });
   }
 
+  // ================================================= //
   openPage(page) {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,15 +7,22 @@ import { MyApp } from "./app.component";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
 
+// ================================================= //
 @NgModule({
+  // ================================================= //
   declarations: [MyApp],
+  // ================================================= //
   imports: [BrowserModule, IonicModule.forRoot(MyApp)],
+  // ================================================= //
   bootstrap: [IonicApp],
+  // ================================================= //
   entryComponents: [MyApp],
+  // ================================================= //
   providers: [
     StatusBar,
     SplashScreen,
     { provide: ErrorHandler, useClass: IonicErrorHandler },
   ],
 })
+// ================================================= //
 export class AppModule {}
