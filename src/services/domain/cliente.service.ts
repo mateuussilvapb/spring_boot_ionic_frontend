d659ce750173145eb5c810980b80137cf0161492
Mateus da Silva Sousa Dias <mateuussilvapb@gmail.com>
2022-06-07 10:29:54 -0300

    Criando a pagina de signup

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -1,7 +1,7 @@
 import { StorageService } from "./../storage.service";
 import { API_CONFIG } from "./../../config/api.config";
 import { Observable } from "rxjs/Rx";
-import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { ClienteDTO } from "../../models/cliente.dto";
 
