4f07680c10667642e3b7a4c91a87708663ae78b9
Mateus da Silva Sousa Dias <41194778+mateuussilvapb@users.noreply.github.com>
2022-07-21 18:41:12 -0300

    Tela de escolha de pagamento

--- src/models/item-pedido.dto.ts
+++ src/models/item-pedido.dto.ts
@@ -1,5 +1,4 @@
 import { RefDTO } from "./ref.dto";
-import { ProdutoDTO } from "./produto.dto";
 // ================================================= //
 export interface ItemPedidoDTO {
   quantidade: number;
