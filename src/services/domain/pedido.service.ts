4efc1084be659a62221fd17c3ce80bc9c4a451f5
Mateus da Silva Sousa Dias <41194778+mateuussilvapb@users.noreply.github.com>
2022-07-22 10:10:36 -0300

    Salvando pedido

---
+++ src/services/domain/pedido.service.ts
@@ -0,0 +1,18 @@
+import { API_CONFIG } from "./../../config/api.config";
+import { PedidoDTO } from "./../../models/pedido.dto";
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+
+// ================================================= //
+@Injectable()
+export class PedidoService {
+  // ================================================= //
+  constructor(public http: HttpClient) {}
+  // ================================================= //
+  insert(obj: PedidoDTO) {
+    return this.http.post(`${API_CONFIG.baseUrl}/pedidos`, obj, {
+      observe: "response",
+      responseType: "text",
+    });
+  }
+}
