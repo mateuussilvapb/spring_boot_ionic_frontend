5bd73646031ed9b0949f7bbcfc17b9536c58680d
Mateus da Silva Sousa Dias <41194778+mateuussilvapb@users.noreply.github.com>
2022-07-22 09:50:31 -0300

    Tela de confirmacao de pedido

--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -29,4 +29,8 @@ export class ClienteService {
       responseType: "text",
     });
   }
+  // ================================================= //
+  findById(id: string) {
+    return this.http.get(`${API_CONFIG.baseUrl}/clientes/${id}`);
+  }
 }
